48db3398dc10959d17a966b18fe37803870d0242
LbnMahs <labanmahs2002@gmail.com>
2023-06-17 21:29:24 +0300

    Theming

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,20 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:expense_tracker/models/expense.dart';
 
-class Expense extends StatefulWidget{
-  const Expense({ super.key });
+class Expenses extends StatefulWidget{
+  const Expenses({ super.key });
 
   @override
-  State<Expense> createState() {
-    return _Expense();
+  State<Expenses> createState() {
+    return _Expenses();
   }
 }
 
-class _Expense extends State<Expense>{
+class _Expenses extends State<Expenses>{
+  final List<Expense> _expenseList = [
+    Expense(
+      title: 'Bread',
+      amount: 60.0,
+      date: DateTime.now(),
+      category: Category.food
+    ),
+    Expense(
+      title: 'Louis V Bag', 
+      amount: 500, 
+      date: DateTime.now(), 
+      category: Category.travel
+    )
+  ];
+
   @override
   Widget build(BuildContext context) {
+    
     return Scaffold(
       appBar: AppBar(
-        title: Text('Expense Tracker'),
+        title: const Text('Expense Tracker'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.add),
+          )
+        ],
       ),
       body: const Center(
         child: Column(
