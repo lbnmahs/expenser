5351972bab75c00d322dba01f13ae9518d20e964
LbnMahs <labanmahs2002@gmail.com>
2023-06-22 19:02:45 +0300

    Configuring the chart bar

---
+++ lib/widgets/chart/chart_bar.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class ChartBar extends StatelessWidget {
+  const ChartBar({ super.key, required this.fill });
+
+  final double fill;
+
+  @override
+  Widget build(BuildContext context) {
+    final isDarkMode = MediaQuery.of(context).platformBrightness == Brightness.dark;
+
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: FractionallySizedBox(
+          heightFactor: fill,
+          child: DecoratedBox(
+            decoration: BoxDecoration(
+              shape: BoxShape.rectangle,
+              borderRadius: BorderRadius.circular(10),
+              color: isDarkMode 
+              ? Theme.of(context).colorScheme.secondary 
+              : Theme.of(context).colorScheme.primary.withOpacity(0.5)
+            )
+          ),
+        ),
+      )
+    );
+  }
+}
